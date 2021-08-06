95899fd4640042463da45f23908805e1a8c5f1c4
Matthew Pierce <lesbianhaircut@outlook.com>
2021-08-05 21:09:59 -0400

    comit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+  function toggleDropdown() {
+    event.target.classList.toggle("colorDark");
+    let sibling = event.target.nextElementSibling;
+    sibling.classList.toggle("hideDisplay");
+  }
+  let listItem = document.querySelectorAll("li>p:nth-of-type(1)");
+  console.log(listItem);
+  listItem.forEach((item) => {
+    item.addEventListener("click", toggleDropdown);
+  });
+});
