41dbd35dab47e46a7607a4f64b3db51fe3efbb8d
Matthew Pierce <lesbianhaircut@outlook.com>
2021-08-05 21:29:23 -0400

    comit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
   function toggleDropdown() {
     event.target.classList.toggle("colorDark");
+    let imageIcon = event.target.lastChild.previousElementSibling;
+    imageIcon.classList.toggle("rotateIcon");
     let sibling = event.target.nextElementSibling;
     sibling.classList.toggle("hideDisplay");
   }
